9a9348e78edad9fbc741011997af6f57c83b3fb1
kissmeter <34291710+kissmeter@users.noreply.github.com>
2017-12-06 10:35:18 +0800

    Add files via upload

---
+++ MyGameForExam/Assets/CreatSomthingScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CreatSomthingScript : MonoBehaviour {
+    [SerializeField] InputField Xinshoujihao;
+    [SerializeField] InputField XinMiMa;
+    [SerializeField] InputField YanZhengMa;
+    [SerializeField] GameObject AnothorPlane;
+    [SerializeField] GameObject ThisPlane;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void StartToZhuce() {
+
+    }
+    public void GoBack() {
+        ThisPlane.SetActive(true);
+        AnothorPlane .SetActive(false);
+
+    }
+}
